dfc5731539f9b335bf8733fcc7da0f76d7692f42
mehmet can <mehmetcan.karabulut@outlook.com.tr>
2024-05-12 12:15:16 +0300

    api port changed to 3004

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = 3004;
 
 // json formatında gelen body'leri parse etmek için
 app.use(express.json());
