1c573ca2b35e68f8a3547c497be310995084f600
mehmet can <mehmetcan.karabulut@outlook.com.tr>
2024-05-12 11:57:52 +0300

    appone service project initialized

---
+++ src/app.js
@@ -0,0 +1,21 @@
+
+const express = require('express');
+const app = express();
+const port = 3001;
+
+// json formatında gelen body'leri parse etmek için
+app.use(express.json());
+
+// GET isteği için geri döndürülecek basit bir JSON verisi.
+let data = [
+    { id: 1, name: "Name 1" },
+    { id: 2, name: "Name 2" },
+];
+
+app.get('/api/data', (req, res) => {
+    res.status(200).json(data);
+});
+
+app.listen(port, () => {
+    console.log(`API http://localhost:${port} serving ...`);
+});
